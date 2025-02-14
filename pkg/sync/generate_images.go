e2c07040812cd1f0eaf9eb733c6b2d0be4bf00d3
Chris Suszyński <ksuszyns@redhat.com>
2025-02-14 19:50:15 +0100

    Use dockerfilegen from o-k/hack

--- pkg/sync/generate_images.go
+++ pkg/sync/generate_images.go
@@ -1,7 +1,14 @@
 package sync
 
+import (
+	"github.com/openshift-knative/hack/pkg/dockerfilegen"
+)
+
 func (o Operation) generateImages() error {
 	o.Println("- Generating images")
 
-	return nil
+	p := dockerfilegen.DefaultParams(o.Path)
+	p.ScanImports = true
+
+	return dockerfilegen.GenerateDockerfiles(p)
 }
